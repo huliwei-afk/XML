1ce8165d3faff2652b5d0717f32462d380df37b3
Alex <395401478@qq.com>
2021-03-17 13:08:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xml/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.xml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.ResponseCache;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.send_request);
+        responseText = findViewById(R.id.response_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequestWithOkHttp();
+            }
+        });
+
+    }
+
+    private void sendRequestWithOkHttp() {
+        //开启线程发送网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url("http://192.168.1.104:8088/get_data.xml").build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    parseXMLWithPull(responseData);
+                    showResponse(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //开线程进行UI操作，将结果显示到界面上
+                responseText.setText(response);
+            }
+        });
+    }
+
+    private void parseXMLWithPull(String xmlData){
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:{
+                        if("app".equals(nodeName)){
+                            Log.d("MainActivity","id is " + id);
+                            Log.d("MainActivity","name is " + name);
+                            Log.d("MainActivity","version is " + version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
